00b427f68e046d0e63305081e30f4039d7b9f815
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-09-26 22:48:30 +0100

    made some good progress

--- src/chapter3.lean
+++ src/chapter3.lean
@@ -1,20 +1,90 @@
-import tactic --imports extra tactics from mathlib
+import tactic
 
 namespace chapter3
 
   open string
   open nat
   open option
+  open function
 
-  inductive tree 
-  | node : tree
-  | ref : string → tree
-  | app : tree → tree → tree
-  
-  namespace tree
-    notation `𝕋` := tree --"\te" --(added to "lean.input.customTranslations" in vscode settings)
-    notation `△` := node --"\nd"
-    infixl `⬝`:60 := app --"\ap"
+  --define natural trees
+  inductive natree 
+  | node : natree
+  | ref : string → natree
+  | app : natree → natree → natree
+  open natree
+
+  --introduce notation
+  notation `𝕋` := natree
+  notation `△` := natree.node
+  infixl `⬝`:60 := natree.app
+
+  variables {α β γ : Type*}
+  variables {w x y z : 𝕋}
+
+  --definition of injective binary functions
+  def binary_injective (f : α → β → γ) : Prop :=
+    (injective f) ∧ (∀ a : α, injective (f a))
+  theorem uncurry_binary_injective (f : α → β → γ) : 
+    binary_injective f ↔ injective (uncurry f) := 
+    begin
+      split,
+        rw [uncurry, injective, binary_injective, injective],
+        intros h p₁ p₂ h₂,
+        cases h,
+        have h₃ : f p₁.fst p₁.snd = f p₂.fst p₂.snd := h₂, --any way to avoid this???
+        have h₄ := h_right p₁.fst,
+        rw injective at h₄,
+        have h₅ := h_right p₂.fst,
+        rw injective at h₅,
+
+    end
+
+  example (a c : α) (b d : β) (h : a = c) (h₂ : b = d) : (a × b) = (c × d) := sorry
+
+  --equational axioms
+  @[simp] axiom kernel : △⬝△⬝y⬝z = y
+  @[simp] axiom stem : △⬝(△⬝x)⬝y⬝z = y⬝z⬝(x⬝z)
+  @[simp] axiom fork : △⬝(△⬝w⬝x)⬝y⬝z = z⬝w⬝x
+
+  --congruence axioms
+  def cong_node : node = node := rfl
+  axiom cong_app : binary_injective (⬝)
+
+  --define primitive combinators
+  @[simp] def K := △⬝△
+  theorem r_K : K⬝y⬝z = y := by simp
+
+  @[simp] def I := △⬝(△⬝△)⬝(△⬝△)
+  theorem r_I : I⬝x = x := by simp
+
+  @[simp] def D := △⬝(△⬝△)⬝(△⬝△⬝△)
+  theorem r_D : D⬝x⬝y⬝z = y⬝z⬝(x⬝z) := by simp
+
+  @[simp] def d (x : 𝕋) := △⬝(△⬝x)
+  theorem d_eq_r_D : d x = D⬝x := by simp
+
+  --derivation of S combinator
+  theorem S_exists : ∃ s : 𝕋, (s⬝x⬝y⬝z = x⬝z⬝(y⬝z)) :=
+  begin
+    split,
+    rw ←r_D,
+    apply congr_fun,
+    sorry,
+    sorry,
+  end
+
+  #check @injective
+
+  #check @congr_fun
+  example (α β : Type*) (f g : α → β) : (∀ z : α, f z = g z) → f = g := by exact funext
+  example (α β : Type*) (f g : α → β) : f = g → (∀ z : α, f z = g z) := by exact congr_fun
+
+  @[simp] def S := d(K⬝D) ⬝ (d K ⬝ (K⬝D))
+  theorem r_S : S⬝x⬝y⬝z = x⬝z⬝(y⬝z) := by simp
+
+  --define associated functions
+  namespace natree
 
     def iterate : 𝕋 → 𝕋 → ℕ → 𝕋
     | t₁ t₂ 0 := t₂
@@ -33,7 +103,7 @@ namespace chapter3
     | (t₁ ⬝ t₂) := max t₁.depth t₂.depth + 1
     | _ := 0
 
-    lemma depth_well_founded {t t₁ t₂ : 𝕋} (h : t = t₁ ⬝ t₂) : t₁.depth < t.depth ∧ t₂.depth < t.depth :=
+    lemma depth_well_founded (h : z = x ⬝ y) : x.depth < z.depth ∧ y.depth < z.depth :=
     begin
       split;
       conv
@@ -42,9 +112,9 @@ namespace chapter3
         rw [h, depth],
       end;
       apply lt_of_le_of_lt,
-        exact le_max_left t₁.depth t₂.depth,
+        exact le_max_left x.depth y.depth,
         apply lt_add_one,
-      exact le_max_right t₁.depth t₂.depth,
+      exact le_max_right x.depth y.depth,
       apply lt_add_one,
     end
 
@@ -52,24 +122,25 @@ namespace chapter3
     | t := 
       match reduce t with
       | some t' := some t'
-      | none := 
-        match t with
-        | △ := none
-        | ref _ := none
-        | (t₁ ⬝ t₂) :=
-          have t₁.depth < t.depth, from (depth_well_founded (eq.refl t)).1,
-          match step t₁ with
-          | some t₁' := some (t₁' ⬝ t₂)
-          | none := 
-            have t₂.depth < t.depth, from (depth_well_founded (eq.refl t)).2,
-            match step t₂ with
-            | some t₂' := some (t₁ ⬝ t₂')
-            | none := none
+      | none :=
+        begin
+          cases h₁ : t with _ t₁ t₂,
+          exact none, exact none, --no reduction in "node" and "ref" cases
+          have h₂ := depth_well_founded h₁, cases h₂, --establish safe recursion hypotheses
+          exact (
+            match step t₁ with
+            | some t₁' := some (t₁' ⬝ t₂)
+            | none :=
+              match step t₂ with
+              | some t₂' := some (t₁ ⬝ t₂')
+              | none := none
+              end
             end
-          end
+          ),
         end
       end
-    using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf depth⟩]} --use "depth" function in well-founded recursion checking
+    --use "depth" function in well-founded recursion checking
+    using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf depth⟩]}
 
     def normalize : ℕ → 𝕋 → 𝕋
     | 0 t := t
@@ -79,23 +150,6 @@ namespace chapter3
       | none := t
       end
 
-  end tree
-
-  #check is_true
-
-  open tree
-
-  def K := △⬝△
-  def I := △⬝(△⬝△)⬝(△⬝△)
-  def D := △⬝(△⬝△)⬝(△⬝△⬝△)
+  end natree
 
 end chapter3
-
--- So I have the following circumstance:
--- ```
--- match t with
---   | (t₁ ⬝ t₂) := ... --need to obtain (h : t = t₁ ⬝ t₂)
---   ...
--- end
--- ```
--- how can I do this?
---
+++ src/misc.lean
@@ -0,0 +1,85 @@
+import tactic --imports extra tactics from mathlib (good to import by default)
+
+------------------------------------------------------------------------------------------
+--Check mathlib is working
+
+import topology.basic
+
+#check topological_space
+
+------------------------------------------------------------------------------------------
+--Establishing equality of terms inside a match block
+
+--https://leanprover.zulipchat.com/#narrow/stream/113488-general/topic/match.20hypothesis
+
+example (b : bool) : {x : bool // x = tt} :=
+match b with
+| tt := ⟨tt, rfl⟩ --we want to be able to use some "h : b = tt" here, but we can't :(
+| ff := ⟨tt, rfl⟩
+end
+
+-- "{x : bool // x = tt}" is subtype of bool with values that satisfy predicate "= tt"
+-- details on subtype here: https://leanprover.github.io/theorem_proving_in_lean/inductive_types.html
+
+--so we can use tactics
+example (b : bool) : {x : bool // x = tt} :=
+begin
+  cases h : b with x y,
+  exact ⟨tt, rfl⟩,
+  exact ⟨b, h⟩ --now we can use our "h : b = tt"
+end
+
+--or we can use a (clever) explicit motive to "rec.on" for the relevant type
+example (b : bool) : {x : bool // x = tt} := --inital
+bool.rec_on 
+  b
+  ⟨tt, rfl⟩
+  ⟨tt, rfl⟩
+
+example (b : bool) : {x : bool // x = tt} := --use explicit motive
+@bool.rec_on 
+  (λ b', {x : bool // x = tt})
+  b
+  ⟨tt, rfl⟩
+  ⟨tt, rfl⟩
+
+example (b : bool) : {x : bool // x = tt} := --add "b = b' →" to beginning of motive and feed "rfl" in at the end
+(
+  @bool.rec_on 
+    (λ b', b = b' → {x : bool // x = tt})
+    b
+    (λ _, ⟨tt, rfl⟩)
+    (λ h, ⟨b, h⟩) --now we can use our "h : b = tt"
+) rfl
+
+------------------------------------------------------------------------------------------
+--Various dependent pair types
+
+#check @Exists  --produces Prop
+#check @psigma  --produces Type*
+#check @subtype --psigma with Sort u_2 = Prop
+#check @sigma   --psigma with Sort u_1 = Type*, Sort u_2 = Type*
+
+universes u v
+structure psigma' {α : Sort u} (β : α → Sort v) : Sort (max u v) :=
+mk :: (fst : α) (snd : β fst)
+
+--psigma' above is disallowed because lean needs to know statically whether something will
+--return a Prop or a Type* (i.e. this cannot depend dynamically on the values passed in)
+
+------------------------------------------------------------------------------------------
+--Well founded recursion: https://leanprover-community.github.io/extras/well_founded_recursion.html
+
+--For inductive types, there is a default-implemented function sizeof
+--which will return the total number of constructors used in a given value of this type
+#check @sizeof --Π {α : Sort u_1} [s : has_sizeof α], α → ℕ
+
+--This is the type used by the well-founded recursion checker
+
+--Essentially, at each recursive call site, Lean needs to establish that
+--t'.sizeof < t.sizeof where t' is the term at the recursive site and t is the original term
+--If it can't do this, you must explicitly provide this as a hypothesis using "have h : t'.sizeof < t.sizeof := ..." 
+--Alternatively, you can produce a different function from your inductive type to the natural numbers
+--f : t → ℕ
+--and use the following tactic at the very end of your recursive function:
+--using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf f⟩]}
